b3d77594262238bc0b6197b0ba388eb5fcc31214
KEN20960 <muchirinephat5@gmail.com>
2022-05-23 12:19:40 +0300

    Thymeleaf

--- src/main/java/com/kcbgroup/main/models/Customer.java
+++ src/main/java/com/kcbgroup/main/models/Customer.java
@@ -21,6 +21,46 @@ public class Customer {
 	private String LAST_NAME;
 	@Column
 	private String ADDRESS;
-	
-	
+
+	public Customer(String FIRST_NAME, String LAST_NAME, String ADDRESS) {
+		this.FIRST_NAME = FIRST_NAME;
+		this.LAST_NAME = LAST_NAME;
+		this.ADDRESS = ADDRESS;
+	}
+
+	public Customer() {
+
+	}
+
+	public int getCID() {
+		return CID;
+	}
+
+	public void setCID(int CID) {
+		this.CID = CID;
+	}
+
+	public String getFIRST_NAME() {
+		return FIRST_NAME;
+	}
+
+	public void setFIRST_NAME(String FIRST_NAME) {
+		this.FIRST_NAME = FIRST_NAME;
+	}
+
+	public String getLAST_NAME() {
+		return LAST_NAME;
+	}
+
+	public void setLAST_NAME(String LAST_NAME) {
+		this.LAST_NAME = LAST_NAME;
+	}
+
+	public String getADDRESS() {
+		return ADDRESS;
+	}
+
+	public void setADDRESS(String ADDRESS) {
+		this.ADDRESS = ADDRESS;
+	}
 }
