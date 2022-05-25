2ac92155687405da3bb3b0ae564234add65489f5
KEN20960 <muchirinephat5@gmail.com>
2022-05-25 11:30:34 +0300

    JPA Find User

--- src/main/java/com/kcbgroup/main/jpa/CustomerJpaRepo.java
+++ src/main/java/com/kcbgroup/main/jpa/CustomerJpaRepo.java
@@ -1,15 +1,24 @@
 package com.kcbgroup.main.jpa;
 
+import com.kcbgroup.main.models.Customer;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
 public class CustomerJpaRepo {
-
     /* TODO: Transactions should be moved to the business services level
          and not repository level*/
 
+    // Connect to the database
+    // Entity manager manages the entities.
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public Customer findById(int id){
+        return entityManager.find(Customer.class, id);
+    }
 }
